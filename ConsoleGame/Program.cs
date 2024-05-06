5dca3e8809fb0ebb15c353737b421ad04e11553c
Elrew <71288819+Elrew@users.noreply.github.com>
2024-05-06 16:25:59 -0700

    Add files via upload

--- ConsoleGame/Program.cs
+++ ConsoleGame/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ConsoleGame
+﻿using static System.Formats.Asn1.AsnWriter;
+
+namespace ConsoleGame
 {
     internal class Program
     {
@@ -6,14 +8,49 @@
         {
             // prep shit
             bool running = true;
-            Console.SetWindowSize(122,35 );
+            Console.SetWindowSize(122, 35);
+            Console.BufferHeight = 35;
+            Console.BufferWidth = 122;
 
-            
             var border = new Maps();
             var playerSprite = new PlayerSprite(50, 13);
             var Menus = new Menus();
             var cutscene = new Cutscenes();
             var mapSprites = new Sprites();
+            string activeScene = "forest";
+
+            Dictionary<string, Scenes> scenes = new Dictionary<string, Scenes>();
+            scenes.Add("forest", new Scenes("Forest", () =>
+            {
+
+                scenes["forest"].IsActive = true;
+            }, () =>
+            {
+
+                scenes["forest"].IsActive = false;
+            }, true));
+
+            scenes.Add("cave", new Scenes("Cave", () =>
+            {
+
+            }, () =>
+            {
+
+            }, true));
+            scenes.Add("house1", new Scenes("House", () =>
+            {
+
+            }, () =>
+            {
+
+            }, true));
+            scenes.Add("caveman", new Scenes("CavemanCS", () =>
+            {
+
+            }, () =>
+            {
+
+            }, true));
 
             Cutscenes.DrawScene(cutscene.opening, 0, 0);
 
@@ -27,6 +64,7 @@
 
             // Title Card
             Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.CursorVisible = false;
             Cutscenes.DrawScene(cutscene.titleCard, 0, 0);
             Console.SetCursorPosition(55, 20);
             Console.WriteLine("Press ENTER");
@@ -40,34 +78,89 @@
             Console.WriteLine("                               ");
 
             // first drawing of shit
-            playerSprite.Draw();          
+            playerSprite.Draw();
             border.DrawBorder();
+            scenes["forest"].Enter();
+            mapSprites.Draw(mapSprites.cave, 25, 110);
+            mapSprites.Draw(mapSprites.house1, 3, 40);
             Menus.MainStats(characterSheet);
-            Menus.DrawMenus(Menus.X,Menus.Y );
-            foreach(var forest1 in border.forest1)
+            border.DrawBorder();
+            playerSprite.Draw();
+            Menus.DrawMenus(Menus.X, Menus.Y);
+            foreach (var forest1 in border.forest1)
             {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
                 mapSprites.Draw(mapSprites.tree1, forest1.Key, forest1.Value);
+                Console.ResetColor();
             }
-            
+
 
             while (running)
-            {   
+            {
+                Console.CursorVisible = false;
                 // draws the forest 1 sprites
-                foreach (var forest1 in border.forest1)
-                {
-                    Console.CursorVisible = false;
-                    mapSprites.Draw(mapSprites.tree1, forest1.Key, forest1.Value);
-                }
 
+                // draw the scene only while the forest scene is active here. Doing so by putting it in a while loop and using scene.IsActive bool
+                if (scenes["forest"].IsActive)
+                {
+                    foreach (var forest1 in border.forest1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        mapSprites.Draw(mapSprites.tree1, forest1.Key, forest1.Value);
+                        Console.ResetColor();
+                        mapSprites.Draw(mapSprites.cave, 25, 110);
+                        mapSprites.Draw(mapSprites.house1, 3, 40);
+
+                        // for debugging position. Don't leave it on, causes memory leak
+                        //Console.SetCursorPosition(10, 3);
+                        //Console.Write("Y:" +playerSprite.posY);
+                        //Console.SetCursorPosition(10, 3);
+                        //Console.Write("X:"+playerSprite.posX);
+                        if ((playerSprite.posX >= 108 && playerSprite.posX <= 111) && playerSprite.posY == 25) { scenes[activeScene].IsActive = false; scenes["cave"].Enter(); }
+                        if ((playerSprite.posX >= 35 && playerSprite.posX <= 43) && (playerSprite.posY >= 2 && playerSprite.posY <= 4)) { scenes[activeScene].IsActive = false; scenes["house1"].Enter(); }
+                    }
+                }
+                // checks play pos for cave enter
+
+                if (scenes["cave"].IsActive)
+                {
+
+                    activeScene = "cave";
+                    Menus.DrawMenus(Menus.X, Menus.Y);
+
+                    if((playerSprite.posX >= 35 && playerSprite.posX <= 43) && (playerSprite.posY >= 2 && playerSprite.posY <= 4)) 
+                    {
+                        Console.Clear();
+                        Cutscenes.DrawScene(cutscene.caveman,0,0);
+                        Console.ReadKey();
+                    }
+                    foreach (var forest1 in border.forest1)
+                    {
+
+                        mapSprites.Draw(mapSprites.caveman, 4, 100);
+                        mapSprites.Draw(mapSprites.cave, 10, 10);
+
+
+
+
+
+                        // for debugging position. Don't leave it on, causes memory leak
+                        //Console.SetCursorPosition(9, 10);
+                        //Console.Write("Y:" + playerSprite.posY);
+                        //Console.SetCursorPosition(10, 11);
+                        //Console.Write("X:" + playerSprite.posX);
+
+                    }
+                }
                 if (Console.KeyAvailable)
                 {
                     var key = Console.ReadKey(true).Key;
-                    border.DrawBorder(); 
+                    border.DrawBorder();
                     playerSprite.Move(key);
                     playerSprite.Draw();
                 }
 
-                
+
 
 
 
---
+++ ConsoleGame/Scenes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGame
+{
+    internal class Scenes
+    {
+        public string Name { get; set; }
+        public Action OnEnter { get; set; }
+        public Action OnExit { get; set; }
+        public bool IsActive { get; set; }
+
+
+        public Scenes(string name, Action onEnter, Action onExit,bool IsActive)
+        {
+            Name = name;
+            OnEnter = onEnter;
+            OnExit = onExit;
+            IsActive = false;
+        }
+
+        public void Enter()
+        {
+            Console.Clear();
+            IsActive= true;
+            OnEnter?.Invoke();
+        }
+
+        public void Exit()
+        {
+            IsActive= false;
+            OnExit?.Invoke();
+        }
+    }
+
+
+}
